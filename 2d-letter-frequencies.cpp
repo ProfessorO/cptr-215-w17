87817b17a030dbaac96617320dbe0a5e751d00df
Robert Ordóñez <rordonez@southern.edu>
2017-01-31 15:13:56 -0500

    Finish letter frequencies, start Lab 3

--- 2d-letter-frequencies.cpp
+++ 2d-letter-frequencies.cpp
@@ -7,17 +7,40 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cctype>
 using namespace std;
 
 int main()
 {
-	string text;
+	string text = "";
 
 	cout << "Enter some text: ";
-	cin >> text; // text = "The quick brown fox ..."
+	getline(cin, text); // text = "The quick brown fox ..."
 
-	vector<int> letterFreq(26);
+	vector<int> letterFreq(26, 0);
 
+	// fill in frequency table
+	/* What I have:
+		text
+			"The quick brown fox jumps over the lazy dog."
+		letterFreq
+			 0  1  2  3  4  5  6  7 ...
+			[0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0]
+			 a  b  c  d
+	*/
+	// for a vector, I'd use vector<int>::size_type
+	for (string::size_type textIndex = 0; textIndex < text.length(); textIndex++)
+	{
+		char letter = tolower(text.at(textIndex));
+		if (isalpha(letter)) // can't Python: 'a' <= letter <= 'z'
+		{
+			int letterIndex = letter - 'a';
+			letterFreq.at(letterIndex)++;
+		}
+	}
+
+	// output letter frequency table
+	// for (int i = 0; i < letterFreq.size(); i++) { ... letterFreq[i] ... }
 	for (char letter = 'a'; letter <= 'z'; letter++)
 	{
 		cout << letter << ": " << letterFreq[letter - 'a'] << endl;
