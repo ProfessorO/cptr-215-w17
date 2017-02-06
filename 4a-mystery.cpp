e192b4317f346df82e443bcf503fdd8a62b187a9
Robert Ordóñez <rordonez@southern.edu>
2017-02-06 09:18:04 -0500

    Function terminology quiz

---
+++ 4a-mystery.cpp
@@ -0,0 +1,34 @@
+/*	4a-mystery.cpp
+	Robert Ordóñez & _________________________
+	2017-02-06
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<unsigned> factorsOf(unsigned num);
+
+int main()
+{
+	cout << "Enter a positive integer: ";
+	unsigned number;
+	cin >> number;	// I enter 12
+	auto factors = factorsOf(number);
+	cout << number << " has " << factors.size()
+		<< " factors." << endl;
+	return 0;
+}
+
+vector<unsigned> factorsOf(unsigned num)
+{
+	vector<unsigned> factors;
+	for (unsigned i = 1; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			factors.push_back(i);
+		}
+	}
+	return factors;
+}
