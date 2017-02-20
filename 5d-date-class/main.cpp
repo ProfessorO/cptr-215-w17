76f47f8dcfb97b75e81778f5e58bacca70ae6df2
Robert Ordóñez <rordonez@southern.edu>
2017-02-20 08:59:54 -0500

    Add constructor and day of week functions

--- 5d-date-class/main.cpp
+++ 5d-date-class/main.cpp
@@ -1,9 +1,12 @@
 /* Date calculator
  * Robert Ordóñez & CPTR-215
- * 2017-02-17
+ * 2017-02-20 -
+ * 2017-02-17 - first draft
  */
 
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Date
@@ -12,29 +15,57 @@ private:
     int year, month, day;
     // int daysSinceEpoch; // since January 1, 1970
 public:
+    Date(int y, int m, int d)
+    {
+        cout << "Constructor called with "
+             << y << ", " << m << ", and " << d << endl;
+        year = y;
+        month = m;
+        day = d;
+    }
+
     void print()
     {
         cout << year << '-' << month << '-' << day;
     }
+
+    int dayOfWeekNumber()
+    {
+        // Zeller's congruence
+        // https://en.wikipedia.org/wiki/Zeller's_congruence#Implementation_in_software
+        int m = month, y = year;
+        if (month <= 2)
+        {
+            m += 12;
+            y -= 1;
+        }
+        return (day + (13 * (m + 1) / 5) + (y % 100) +
+                ((y % 100) / 4) + ((y / 100) / 4) +
+                (5 * (y / 100))) % 7;
+    }
+
+    string dayOfWeekName()
+    {
+        vector<string> dayNames({"Saturday", "Sunday", "Monday", "Tuesday",
+                                 "Wednesday", "Thursday", "Friday"});
+        return dayNames.at(dayOfWeekNumber());
+    }
 };
 
 int daysBetween(Date date1, Date date2);
-void printDate(Date date);
 
 int main()
 {
-    Date today, epoch = { 1970, 1, 1 };
+    Date today(2017, 2, 20);
+    Date epoch(1970, 1, 1);
 
-    today.year = 2017;
-    today.month = 2;
-    today.day = 17;
-
+    cout << today.dayOfWeekName() << " ";
     today.print();
     cout << endl;
-    printDate(epoch); // epoch.print();
+    cout << epoch.dayOfWeekName() << " ";
+    epoch.print();
     cout << endl;
 
-    cout << today.year << "-" << today.month << "-" << today.day << endl;
     cout << "Days between those dates: " << daysBetween(today, epoch) << endl;
 
 //    cin >> date1 >> date2;
@@ -47,9 +78,4 @@ int daysBetween(Date date1, Date date2)
 {
     // TODO: write this function
     return 0;
-}
-
-void printDate(Date date)
-{
-    cout << date.year << '-' << date.month << '-' << date.day;
 }
